fdd6afd92ade1d39d162797ff21a83df26918b45
RyanMcKeee <80672291+RyanMcKeee@users.noreply.github.com>
2021-03-15 03:45:31 -0400

    Update createStages.lua

--- createStages.lua
+++ createStages.lua
@@ -2,7 +2,7 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 -- Variables
-local Stages = ReplicatedStorage:WaitForChild("Stages)
+local Stages = ReplicatedStorage:WaitForChild("Stages")
 local DefaultStages = Stages:WaitForChild("DefaultStages")
 local StageTemplate = script.Parent:WaitForChild("StageTemplate")
 
