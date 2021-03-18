27abe991d24ed2d4fe452c8ab9785d5cd86d949d
RyanMcKeee <80672291+RyanMcKeee@users.noreply.github.com>
2021-03-18 09:10:03 -0400

    Create clearconnections.lua

---
+++ clearconnections.lua
@@ -0,0 +1,22 @@
+local Connections = {}
+
+function ClearAllConnections()
+	for i, v in pairs(Connections) do
+		v:Disconnect()
+		Connections[i] = nil
+	end
+end
+
+function ClearSpecificConnection(Key)
+	local Connection = Connections[Key]
+	if Connection then
+		Connection:Disconnect()
+		Connections[Key] = nil
+	end
+end
+
+-- Connections.OnButtonPressed = Button.Activated:Connect(ButtonPressed)
+-- function ButtonPressed()
+-- 	 print("Button has been pressed!")
+-- end
+-- ClearAllConnections()	// This would delete the button's connection
